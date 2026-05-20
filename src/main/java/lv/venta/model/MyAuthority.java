5ebdeb695d5ecaa45fbfb1b5eea3a88786c75d62
WIN-V7UE9F5T3LF\user <s23dudovine@venta.lv>
2026-05-20 10:57:30 +0300

    Added repo and updated model

--- src/main/java/lv/venta/model/MyAuthority.java
+++ src/main/java/lv/venta/model/MyAuthority.java
@@ -1,10 +1,14 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -14,7 +18,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
+//git checkout master
+//git pull origin master
+//gir branch 
+//git merge ritvars
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,19 +29,25 @@ import lombok.ToString;
 @Table(name = "MyAuthorityTable")
 @Entity
 public class MyAuthority {
-
+	
 	@Setter(value = AccessLevel.NONE)
 	@Column(name = "Ida")
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long ida;
 	
 	@Column(name = "Title")
 	@NotNull
 	@NotEmpty
-	@Pattern(regexp = "[A-Z_]{4-10}")
+	@Pattern(regexp = "[A-Z_]{4,10}")
 	private String title;
 	
+	@OneToMany(mappedBy =  "authority")
+	@ToString.Exclude
+	private Collection<MyUser> users = new ArrayList<>();
 	
+	public MyAuthority(String title) {
+		setTitle(title);
+	}
 	
 }
